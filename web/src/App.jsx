1d0c58f045fa4b4cee11251def8d364e2ae05ea9
ThayalanGR <grthayalan18@gmail.com>
2019-10-27 22:45:57 +0530

    openbeats UI design upto playlist holder done

---
+++ web/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./css/main.css";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        OpenBeats
+      </div>
+    )
+  }
+}
