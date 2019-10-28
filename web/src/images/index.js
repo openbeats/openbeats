23145e070b66891b6cc198fe19ad220fc9174568
ThayalanGR <grthayalan18@gmail.com>
2019-10-28 22:59:33 +0530

    music player layout implementation, search result layout.. dummy layout placeholder done

--- web/src/images/index.js
+++ web/src/images/index.js
@@ -1,7 +1,16 @@
 import musicNote from "./music-note.png";
-
+import download from "./download.png";
+import downloadOrange from "./download-orange.png";
+import playlist from "./playlist.png";
+import playlistadd from "./playlist-add.png";
+import play from "./playicon.png";
 
 
 export {
-    musicNote
+    musicNote,
+    download,
+    playlist,
+    play,
+    playlistadd,
+    downloadOrange
 }
