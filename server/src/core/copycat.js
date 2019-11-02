1015639c30816b24f1443a604da780bbe34c26ac
ThayalanGR <grthayalan18@gmail.com>
2019-11-03 00:05:45 +0530

    openbeats - opencc copycat api optimization done

--- server/src/core/copycat.js
+++ server/src/core/copycat.js
@@ -4,8 +4,8 @@ import { JSDOM } from 'jsdom';
 
 
 export default async (id, quality) => {
-    let link = `https://www.youtube.com/watch?v=${id}`
     let outputLink = null;
+    let link = `https://www.youtube.com/watch?v=${id}`
     const analyzeLink = "https://www.youtubeconverter.io/convert-mp3";
     const convertLink = "https://www.youtubeconverter.io/convert/index";
     let formData = new FormData();
@@ -17,29 +17,18 @@ export default async (id, quality) => {
         })
         .then(res => res.text())
         .then(async res => {
-            let data = res.split("_id:'");
-            let _id = data[1].substr(0, 24);
-            let v_id = id;
-            let fd = new FormData();
-            fd.append("type", 'youtube');
-            fd.append("_id", _id);
-            fd.append("v_id", v_id);
-            fd.append("ajax", '1');
-            fd.append("ftype", 'mp3');
-            fd.append("fquality", quality);
-            await fetch(convertLink,
-                {
-                    method: 'post',
-                    body: fd,
-                })
-                .then(res => res.json())
-                .then(res => {
-                    let dom = new JSDOM(res.result.trim());
-                    let targetNode = dom.window.document.getElementsByClassName("btn-file");
-                    if (targetNode.length > 0) {
-                        outputLink = targetNode[0].getAttribute("href");
-                    }
-                })
+
+            if (quality == '128') {
+                let dom = new JSDOM(res.trim());
+                let targetNode = dom.window.document.getElementsByClassName("btn-file");
+                if (targetNode.length > 0) {
+                    outputLink = targetNode[0].getAttribute("href");
+                } else {
+                    outputLink = await goodQuality(res, id, quality, convertLink)
+                }
+            } else {
+                outputLink = await goodQuality(res, id, quality, convertLink)
+            }
         })
         .catch(err => console.log(err))
     return outputLink;
@@ -47,3 +36,34 @@ export default async (id, quality) => {
 
 
 
+
+
+async function goodQuality(res, id, quality, convertLink) {
+    let outputLink = null;
+    let data = res.split("_id:'");
+    let _id = data[1].substr(0, 24);
+    let v_id = id;
+    let fd = new FormData();
+    fd.append("type", 'youtube');
+    fd.append("_id", _id);
+    fd.append("v_id", v_id);
+    fd.append("ajax", '1');
+    fd.append("ftype", 'mp3');
+    fd.append("fquality", quality);
+    await fetch(convertLink,
+        {
+            method: 'post',
+            body: fd,
+        })
+        .then(res => res.json())
+        .then(res => {
+            let dom = new JSDOM(res.result.trim());
+            let targetNode = dom.window.document.getElementsByClassName("btn-file");
+            if (targetNode.length > 0) {
+                outputLink = targetNode[0].getAttribute("href");
+            }
+        })
+        .catch(err => console.error(err))
+
+    return outputLink;
+}
