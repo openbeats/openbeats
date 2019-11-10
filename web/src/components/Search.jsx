4f2d363ba1508b646b3088fabc2b71b678cc3646
ThayalanGR <grthayalan18@gmail.com>
2019-11-10 20:06:28 +0530

    search bar suggestion selector navigation added

--- web/src/components/Search.jsx
+++ web/src/components/Search.jsx
@@ -6,7 +6,9 @@ export default class Search extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            suggestionText: ""
+            suggestionText: "",
+            currentTextIndex: 0,
+            actualText: ""
         }
     }
 
@@ -14,17 +16,52 @@ export default class Search extends Component {
         return (
             <div className="main-search-wrapper">
                 <div className="main-search">
-                    <form action="" onSubmit={async (e) => { e.preventDefault(); await this.props.fetchResults(this.state.suggestionText); }}>
-                        <input autoFocus type="text" value={this.state.suggestionText} onChange={async (e) => { this.setState({ suggestionText: e.target.value }); await this.props.getKeywordSuggestion(e) }} className="search-input" placeholder="Search Your Music Here!" />
+                    <form
+                        action=""
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            let a = await this.state.suggestionText;
+                            await this.props.fetchResults(a);
+                        }}>
+                        <input
+                            autoFocus
+                            type="text"
+                            onKeyUp={async (e) => {
+                                if (e.keyCode === 40) {
+                                    let current = (this.state.currentTextIndex + 1) % (this.props.state.keywordSuggestions.length + 1);
+                                    if (current !== 0)
+                                        await this.setState({ currentTextIndex: current, suggestionText: this.props.state.keywordSuggestions[current - 1][0] })
+                                    else
+                                        await this.setState({ currentTextIndex: current, suggestionText: this.state.actualText })
+                                } else if (e.keyCode === 38) {
+                                    let current = Math.abs(this.state.currentTextIndex - 1) % (this.props.state.keywordSuggestions.length + 1);
+                                    if (current !== 0)
+                                        await this.setState({ currentTextIndex: current, suggestionText: this.props.state.keywordSuggestions[current - 1][0] })
+                                    else
+                                        await this.setState({ currentTextIndex: current, suggestionText: this.state.actualText })
+                                }
+                            }}
+                            value={this.state.suggestionText}
+                            onChange={async (e) => {
+                                let a = e.target.value
+                                await this.setState({ suggestionText: a, actualText: a, currentTextIndex: 0 });
+                                await this.props.getKeywordSuggestion(a)
+                            }} className="search-input"
+                            placeholder="Search Your Music Here!"
+                        />
                         <button className="search-icon" type="submit"><i className="fas fa-search"></i></button>
                     </form>
                     {this.props.state.keywordSuggestions.length > 0 &&
                         <div className="suggestion-keyword-holder">
                             {this.props.state.keywordSuggestions.map((item, key) => (
-                                <div onClick={async (e) => {
-                                    await this.setState({ suggestionText: item[0] })
-                                    await this.props.fetchResults(this.state.suggestionText);
-                                }} key={key} className="suggested-keyword" >{item[0]}</div>
+                                <div
+                                    onClick={async (e) => {
+                                        await this.setState({ suggestionText: item[0] })
+                                        await this.props.fetchResults(this.state.suggestionText);
+                                    }}
+                                    key={key}
+                                    className={`suggested-keyword ${this.state.currentTextIndex === key + 1 ? 'highlight-current' : ''}`}
+                                >{item[0]}</div>
                             ))}
                         </div>
                     }
