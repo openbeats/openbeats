917ac5115610f9802bee4df7d09cd7a331785b19
noelimmi <noelimmanuel1998@gmail.com>
2019-11-12 14:52:49 +0530

    Done Scraping and setted up api to fetch charts

--- server/src/core/index.js
+++ server/src/core/index.js
@@ -1,6 +1,5 @@
 import copycat from "./copycat";
 import ytcat from "./ytsearchcat";
 import suggestbeat from "./suggestbeat";
-import scrapmirchi from "./scrapmirchi";
 
-export { scrapmirchi, copycat, ytcat, suggestbeat };
+export { copycat, ytcat, suggestbeat };
