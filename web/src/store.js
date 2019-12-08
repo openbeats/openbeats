fc180a5afcd3ac9ef19b607f0e7d27e55ec0834e
ThayalanGR <grthayalan18@gmail.com>
2019-12-08 10:56:25 +0530

    docker images for server and frontend builded

--- web/src/store.js
+++ web/src/store.js
@@ -1,10 +1,10 @@
-import { createHashHistory } from 'history';
+import { createBrowserHistory } from 'history';
 import { combineReducers, applyMiddleware, createStore } from "redux";
 import { connectRouter, routerMiddleware, ConnectedRouter } from 'connected-react-router';
 import thunk from "redux-thunk";
 // import logger from "redux-logger";
 import reducers from "./reducers";
-const history = createHashHistory();
+const history = createBrowserHistory();
 const store = createStore(
     combineReducers({
         router: connectRouter(history),
