c871dc6ee69745983c4709961b7e1168da741de4
ThayalanGR <grthayalan18@gmail.com>
2019-12-26 15:01:07 +0530

    services refactoring for aggregating backend services

--- services/server/src/config/db.js
+++ services/server/src/config/db.js
@@ -1,23 +1,35 @@
-import mongoose from 'mongoose';
-import { Book } from './schema';
-
+import mongoose from "mongoose";
+import { Book } from "./schema";
+import config from "config";
 
 export default async () => {
-    const mongoUrl = `mongodb://${process.env.DB}/obs-db`
-    await mongoose
-        .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("Connection Successful!"); 
-         
-            // a document instance
-            var book1 = new Book({ name: 'Introduction to Mongoose', price: 10, quantity: 25 });
-         
-            // save model to database
-            book1.save(function (err, book) {
-              if (err) return console.error(err);
-              console.log(book.name + " saved to bookstore collection.");
-            });   
-        })
-        .catch(err => console.error("Mongo db connection failed!", err.message))
+  let mongoUrl = "";
+  if (config.get("isDev")) {
+    mongoUrl = config.get("mongoURI_DEV");
+  } else {
+    mongoUrl = process.env.DB;
+  }
+  await mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    .then(() => {
+      console.log("Connection Successful!");
 
-}
+      // a document instance
+      var book1 = new Book({
+        name: "Introduction to Mongoose",
+        price: 10,
+        quantity: 25
+      });
+
+      // save model to database
+      book1.save(function(err, book) {
+        if (err) return console.error(err);
+        console.log(book.name + " saved to bookstore collection.");
+      });
+    })
+    .catch(err => console.error("Mongo db connection failed!", err.message));
+};
