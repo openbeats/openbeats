dc87a8447282f05c02b2fc08d0586fb34d456661
ThayalanGR <grthayalan18@gmail.com>
2019-12-28 22:07:16 +0530

    auth ui implemented

--- services/clientapp/src/images/index.js
+++ services/clientapp/src/images/index.js
@@ -1,49 +1,49 @@
-import musicDummy from './dummy_music_holder.jpg';
-import masterLogo from './masterlogo.png'
-import navhome from './navhome.png'
-import navchart from './navchart.png'
-import navartist from './navartist.png'
-import navalbum from './navalbum.png'
-import navhistory from './navhistory.png'
-import navplaylist from './navplaylist.png'
-import navplus from './navplus.png'
-import playerdownload from './playerdownload.png'
-import playernext from './playernext.png'
-import playerpause from './playerpause.png'
-import playerplay from './playerplay.png'
-import playerprevious from './playerprevious.png'
-import playerqueue from './playerqueue.png'
-import playervolume from './playervolume.png'
-import angleright from './angleright.png'
-import mainsearch from './mainsearch.png'
-import hamburger from './hamburger.png'
-import navclose from './navclose.png'
-import master from './master.png'
-import playlistadd from './playlistadd.png'
-
-
+import musicDummy from "./dummy_music_holder.jpg";
+import masterLogo from "./masterlogo.png";
+import navhome from "./navhome.png";
+import navchart from "./navchart.png";
+import navartist from "./navartist.png";
+import navalbum from "./navalbum.png";
+import navhistory from "./navhistory.png";
+import navplaylist from "./navplaylist.png";
+import navplus from "./navplus.png";
+import playerdownload from "./playerdownload.png";
+import playernext from "./playernext.png";
+import playerpause from "./playerpause.png";
+import playerplay from "./playerplay.png";
+import playerprevious from "./playerprevious.png";
+import playerqueue from "./playerqueue.png";
+import playervolume from "./playervolume.png";
+import angleright from "./angleright.png";
+import mainsearch from "./mainsearch.png";
+import hamburger from "./hamburger.png";
+import navclose from "./navclose.png";
+import master from "./master.png";
+import playlistadd from "./playlistadd.png";
+import musicIllustration from "./music_illustration.svg";
 
 export {
-    musicDummy,
-    masterLogo,
-    navhome,
-    navchart,
-    navartist,
-    navalbum,
-    navhistory,
-    navplaylist,
-    navplus,
-    playerdownload,
-    playernext,
-    playerpause,
-    playerplay,
-    playerprevious,
-    playerqueue,
-    playervolume,
-    angleright,
-    mainsearch,
-    hamburger,
-    navclose,
-    master,
-    playlistadd
-}
+  musicDummy,
+  masterLogo,
+  navhome,
+  navchart,
+  navartist,
+  navalbum,
+  navhistory,
+  navplaylist,
+  navplus,
+  playerdownload,
+  playernext,
+  playerpause,
+  playerplay,
+  playerprevious,
+  playerqueue,
+  playervolume,
+  angleright,
+  mainsearch,
+  hamburger,
+  navclose,
+  master,
+  playlistadd,
+  musicIllustration
+};
