653b47a184d12673e56f8a6fb4f19c5e3c5a7d18
noelimmi <noelimmanuel1998@gmail.com>
2019-12-28 18:34:12 +0530

    Added auth service

---
+++ services/auth/src/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export default mongoose.model(
+	"User",
+	new mongoose.Schema({
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+		},
+		avatar: {
+			type: String,
+		},
+		date: {
+			type: Date,
+			default: Date.now,
+		},
+	}),
+);
