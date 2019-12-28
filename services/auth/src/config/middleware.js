c1c152ffa3647d00126512a91a842775d958aa91
noelimmi <noelimmanuel1998@gmail.com>
2019-12-28 18:34:12 +0530

    Added auth service

--- services/auth/src/config/middleware.js
+++ services/auth/src/config/middleware.js
@@ -1,10 +1,15 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import passport from "passport";
+import authRoutes from "../routes/auth";
+import passportStrategy from "./passport";
 
 export default app => {
 	app.use(cors());
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(morgan("dev"));
+	app.use(passport.initialize());
+	app.use("/auth", authRoutes);
 };
--- services/auth/src/routes/auth.js
+++ services/auth/src/routes/auth.js
@@ -1,13 +1,69 @@
 import express from "express";
+import User from "../models/User";
+import passport from "passport";
+import jwt from "jsonwebtoken";
+import config from "config";
 import { check, validationResult } from "express-validator";
 import gravatar from "gravatar";
 import bcrypt from "bcryptjs";
-import config from "config";
-import User from "../models/User";
-import jwt from "jsonwebtoken";
 import auth from "../config/auth";
 
 const router = express.Router();
+
+router.get(
+	"/google",
+	passport.authenticate("google", {
+		scope: ["email", "profile"],
+	}),
+);
+
+router.get(
+	"/google/callback",
+	passport.authenticate("google", { session: false }),
+	(req, res) => {
+		const payload = { user: req.user };
+		jwt.sign(
+			payload,
+			config.get("jwtSecret"),
+			{ expiresIn: 360000 },
+			(err, token) => {
+				try {
+					if (err) throw error;
+					res.redirect(302, `https://openbeats.live/auth/callback/${token}`);
+				} catch (error) {
+					res.status(500).json({ error: "Internal Server Error" });
+				}
+			},
+		);
+	},
+);
+
+router.post("/login", (req, res, next) => {
+	passport.authenticate("local", (error, user, info) => {
+		if (error) {
+			console.error(error.message);
+			res.status(500).json({ error: "Internal Server Error" });
+		}
+		if (info !== undefined) {
+			res.status(401).json({ error: info.message });
+		} else {
+			const payload = { user };
+			jwt.sign(
+				payload,
+				config.get("jwtSecret"),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					try {
+						if (err) throw error;
+						res.json({ token });
+					} catch (error) {
+						res.status(500).json({ error: "Internal Server Error" });
+					}
+				},
+			);
+		}
+	})(req, res, next);
+});
 
 router.post(
 	"/register",
@@ -49,6 +105,7 @@ router.post(
 				password,
 				avatar,
 			});
+
 			const salt = await bcrypt.genSalt(config.get("saltRound"));
 			user.password = await bcrypt.hash(password, salt);
 			await user.save();
@@ -82,4 +139,5 @@ router.get("/me", auth, async (req, res) => {
 		res.status(500).send("Server error");
 	}
 });
+
 export default router;
