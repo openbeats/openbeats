deaf0ca87fab654497d45891f66b49ddfeab0ac3
ThayalanGR <grthayalan18@gmail.com>
2019-12-30 18:00:28 +0530

    auth service deployment

--- services/auth/src/config/middleware.js
+++ services/auth/src/config/middleware.js
@@ -2,14 +2,13 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import passport from "passport";
-import authRoutes from "../routes/auth";
 import passportStrategy from "./passport";
 
 export default app => {
+	passportStrategy();
 	app.use(cors());
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(morgan("dev"));
 	app.use(passport.initialize());
-	app.use("/", authRoutes);
 };
--- services/auth/src/routes/auth.js
+++ services/auth/src/routes/auth.js
@@ -11,105 +11,116 @@ import auth from "../config/auth";
 const router = express.Router();
 
 router.post("/login", (req, res, next) => {
-	passport.authenticate("local", (error, user, info) => {
-		if (error) {
-			console.error(error.message);
-			res.status(500).json({ error: "Internal Server Error" });
-		}
-		if (info !== undefined) {
-			res.status(401).json({ error: info.message });
-		} else {
-			const payload = { user };
-			jwt.sign(
-				payload,
-				config.get("jwtSecret"),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					try {
-						if (err) throw error;
-						res.json({ token });
-					} catch (error) {
-						res.status(500).json({ error: "Internal Server Error" });
-					}
-				},
-			);
-		}
-	})(req, res, next);
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      console.error(error.message);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+    if (info !== undefined) {
+      res.status(401).json({ error: info.message });
+    } else {
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          try {
+            if (err) throw error;
+            res.json({
+              token,
+              name: user.name,
+              email: user.email,
+              id: user.id,
+              avatar: user.avatar
+            });
+          } catch (error) {
+            res.status(500).json({ error: "Internal Server Error" });
+          }
+        }
+      );
+    }
+  })(req, res, next);
 });
-
 router.post(
-	"/register",
-	[
-		check("name", "Name is required")
-			.not()
-			.isEmpty(),
-		check("email", "Please include a valid email").isEmail(),
-		check(
-			"password",
-			"Please enter a password with 6 or more characters",
-		).isLength({ min: 6 }),
-	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
+  "/register",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-		const { name, email, password } = req.body;
+    const { name, email, password } = req.body;
 
-		try {
-			let user = await User.findOne({ email });
-			if (user) {
-				return res
-					.status(400)
-					.json({ errors: [{ msg: "User with that email id already exist" }] });
-			}
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User with that email id already exist" }] });
+      }
 
-			const avatar = gravatar.url(email, {
-				s: "200",
-				r: "pg",
-				d: "mm",
-			});
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
 
-			user = new User({
-				name,
-				email,
-				password,
-				avatar,
-			});
+      user = new User({
+        name,
+        email,
+        password,
+        avatar
+      });
 
-			const salt = await bcrypt.genSalt(config.get("saltRound"));
-			user.password = await bcrypt.hash(password, salt);
-			await user.save();
-			const payload = { user: { id: user.id } };
-			jwt.sign(
-				payload,
-				config.get("jwtSecret"),
-				{ expiresIn: 360000 },
-				(err, token) => {
-					try {
-						if (err) throw err;
-						res.json({ token });
-					} catch (error) {
-						res.status(500).send({ error: "Internal Server Error" });
-					}
-				},
-			);
-		} catch (error) {
-			console.error(error.message);
-			res.status(500).send({ error: "Internal Server Error" });
-		}
-	},
+      const salt = await bcrypt.genSalt(config.get("saltRound"));
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      const payload = { user: { id: user.id } };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          try {
+            if (err) throw err;
+            res.json({
+              token,
+              name: user.name,
+              email: user.email,
+              id: user.id,
+              avatar: user.avatar
+            });
+          } catch (error) {
+            res.status(500).send({ error: "Internal Server Error" });
+          }
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
+  }
 );
 
 router.get("/me", auth, async (req, res) => {
-	try {
-		const user = await User.findById(req.user.id).select("-password");
-		res.json(user);
-	} catch (error) {
-		console.error(error.message);
-		res.status(500).send("Server error");
-	}
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 });
 
 export default router;
