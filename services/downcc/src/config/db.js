b47089fbb3bbee0e8055270bd62c032e310df156
noelimmi <noelimmanuel1998@gmail.com>
2020-01-18 12:25:15 +0530

    Fixed video only bug

--- services/downcc/src/config/db.js
+++ services/downcc/src/config/db.js
@@ -2,20 +2,20 @@ import mongoose from "mongoose";
 import config from "config";
 
 export default async () => {
-  let mongoUrl = "";
-  if (config.get("isDev")) {
-    mongoUrl = config.get("mongoURI_DEV");
-  } else {
-    mongoUrl = process.env.DB;
-  }
-  await mongoose
-    .connect(mongoUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    })
-    .then(() => {
-      console.log("Connection Successful!");
-    })
-    .catch(err => console.error("Mongo db connection failed!", err.message));
+	let mongoUrl = "";
+	if (config.get("isDev")) {
+		mongoUrl = config.get("mongoURI_DEV");
+	} else {
+		mongoUrl = process.env.DB;
+	}
+	await mongoose
+		.connect(mongoUrl, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		})
+		.then(() => {
+			console.log("Connection Successful!");
+		})
+		.catch(err => console.error("Mongo db connection failed!", err.message));
 };
