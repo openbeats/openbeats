29594fb64279594b43a976dbc5717b54d292c288
ThayalanGR <grthayalan18@gmail.com>
2020-01-19 00:13:30 +0530

    user playlist service implementation done

---
+++ services/playlist/src/models/UserPlaylist.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+export default mongoose.model(
+	"UserPlaylist",
+	new mongoose.Schema({
+		name: String,
+		songs: [{
+			title: String,
+			thumbnail: String,
+			duration: String,
+			videoId: String,
+			channelName: String,
+			channelId: String,
+			uploadedOn: String,
+			views: String,
+			description: String,
+		}],
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+		},
+		updatedAt: {
+			type: Date,
+			default: Date.now()
+		},
+		createdBy: String
+	}),
+);
---
+++ services/playlist/src/routes/userPlaylist.js
@@ -0,0 +1,223 @@
+import express from "express";
+import UserPlaylist from "../models/UserPlaylist";
+import User from "../models/User";
+
+const router = express.Router();
+
+// create empty Playlist
+router.post("/create", async (req, res) => {
+    try {
+        const {
+            name,
+            userId
+        } = req.body;
+        const userPlaylist = new UserPlaylist({
+            name,
+            createdBy: userId
+        });
+        const playlistData = await userPlaylist.save()
+        const user = await User.findOne({
+            _id: userId
+        })
+        await user.myPlaylists.push({
+            name,
+            playlistId: playlistData._id
+        })
+        const userData = await user.save()
+        res.send({
+            status: true,
+            data: playlistData,
+            d: userData
+        })
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+// add songs into Playlist
+router.post("/addsongs", async (req, res) => {
+    try {
+        const {
+            songs,
+            playlistId
+        } = req.body;
+
+        const playlist = await UserPlaylist.findOne({
+            _id: playlistId
+        });
+
+        await playlist.songs.push(...songs);
+
+        await playlist.updateOne({
+            updatedAt: Date.now()
+        })
+
+        const savedSongs = await playlist.save()
+
+        res.send({
+            status: true,
+            data: savedSongs,
+        })
+
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+
+})
+
+// get all playlist metadata
+router.get("/getallplaylistmetadata/:uid", async (req, res) => {
+    try {
+        const uid = req.params.uid
+        const user = await User.findOne({
+            _id: uid
+        });
+        const playlistMetadata = user.myPlaylists;
+        res.send({
+            status: true,
+            data: playlistMetadata
+        })
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+// get user playlist
+router.get("/getplaylist/:id", async (req, res) => {
+    try {
+        const playlistId = req.params.id;
+        const data = await UserPlaylist.findOne({
+            _id: playlistId
+        });
+
+        res.send({
+            status: true,
+            data
+        })
+
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+// delete songs from playlist
+router.post("/deletesong", async (req, res) => {
+    try {
+
+        const {
+            playlistId,
+            songId
+        } = req.body;
+
+        const playlist = await UserPlaylist.findOne({
+            _id: playlistId
+        });
+
+        await playlist.updateOne({
+            songs: await playlist.songs.filter(item => item._id.toString() !== songId.toString())
+        })
+
+        res.send({
+            status: true,
+            data: "song delete from playlist successfully!"
+        })
+
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+// update name of the playlist
+router.post("/updatename", async (req, res) => {
+
+    try {
+
+        const {
+            name,
+            playlistId
+        } = req.body;
+
+        const playlist = await UserPlaylist.findOne({
+            _id: playlistId
+        });
+
+        const createdBy = playlist.createdBy;
+
+        const user = await User.findOne({
+            _id: createdBy
+        })
+
+
+        await playlist.updateOne({
+            name,
+            updatedAt: Date.now()
+        })
+
+        await user.updateOne({
+            myPlaylists: [...await user.myPlaylists.filter(item => item.playlistId !== playlistId), {
+                name,
+                playlistId
+            }]
+        });
+
+        res.send({
+            status: true,
+            data: "Playlist name changed successfully!",
+        })
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+//delete playlist
+router.get("/delete/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+        const playlist = await UserPlaylist.findOne({
+            _id
+        });
+
+        const userId = playlist.createdBy
+        const user = await User.findOne({
+            _id: userId
+        });
+
+        await user.updateOne({
+            myPlaylists: await user.myPlaylists.filter(item => item.playlistId !== _id)
+        });
+
+        await playlist.deleteOne()
+
+        res.send({
+            status: true,
+            data: "playlist deleted successfully!"
+        })
+
+    } catch (error) {
+        res.send({
+            status: false,
+            data: error.toString()
+        })
+    }
+})
+
+
+export default router;
