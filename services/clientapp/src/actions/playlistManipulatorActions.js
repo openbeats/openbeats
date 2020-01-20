11b4fed5faaad7503868d9222d69e7de760e09d0
ThayalanGR <grthayalan18@gmail.com>
2020-01-20 23:48:34 +0530

    user playlist - empty playlist creation ui flow implemented

--- services/clientapp/src/actions/playlistManipulatorActions.js
+++ services/clientapp/src/actions/playlistManipulatorActions.js
@@ -1,9 +1,13 @@
 import {
     store
 } from "../store";
-// import {
-//     variables
-// } from "../config";
+import axios from "axios";
+import {
+    variables
+} from "../config";
+import {
+    toastActions
+} from ".";
 
 export function showAddPlaylistDialog(song) {
     store.dispatch({
@@ -27,6 +31,59 @@ export function clearAddPlaylistDialog() {
 export function fetchUserPlaylist() {
 
     return true;
+}
+
+export async function fetchUserPlaylistMetadata(userId) {
+    try {
+
+        let metaData = [];
+        const {
+            data
+        } = await axios.get(`${variables.baseUrl}/playlist/userplaylist/getallplaylistmetadata/${userId}`)
+
+        metaData = data.data
+        console.log(metaData);
+
+
+        store.dispatch({
+            type: "SHOW_PLAYLIST_ADD_DIALOG",
+            payload: {
+                userPlaylistMetaData: metaData
+            }
+        })
+
+    } catch (error) {
+        console.error(error)
+    }
+    return true;
+}
+
+export async function addSongToPlaylist(pId, song) {
+    console.log(pId, song);
+    toastActions.showMessage(pId.toString() + " " + song.videoId.toString())
+
+
+}
+export async function removeSongFromPlaylist(pId, song) {
+    console.log(pId, song);
+    toastActions.showMessage(pId.toString() + " " + song.videoId.toString())
+
+}
+
+export async function createNewPlaylist(userId, name) {
+    try {
+        await axios.post(`${variables.baseUrl}/playlist/userplaylist/create`, {
+            name,
+            userId
+        })
+        toastActions.showMessage("Playlist created successfully!")
+        await fetchUserPlaylistMetadata(userId)
+    } catch (error) {
+        toastActions.showMessage(error.toString())
+    }
+
+    return true
+
 }
 
 export function deleteUserPlaylist(params) {
