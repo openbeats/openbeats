4bcd11a7f5664e727fd4e60ce467afbb7f0ab9ad
noelimmi <noelimmanuel1998@gmail.com>
2020-01-25 00:12:05 +0530

    after latest merge form master branch..

--- services/playlist/src/models/UserPlaylist.js
+++ services/playlist/src/models/UserPlaylist.js
@@ -4,17 +4,19 @@ export default mongoose.model(
 	"UserPlaylist",
 	new mongoose.Schema({
 		name: String,
-		songs: [{
-			title: String,
-			thumbnail: String,
-			duration: String,
-			videoId: String,
-			channelName: String,
-			channelId: String,
-			uploadedOn: String,
-			views: String,
-			description: String,
-		}, ],
+		songs: [
+			{
+				title: String,
+				thumbnail: String,
+				duration: String,
+				videoId: String,
+				channelName: String,
+				channelId: String,
+				uploadedOn: String,
+				views: String,
+				description: String,
+			},
+		],
 		createdAt: {
 			type: Date,
 			default: Date.now(),
@@ -24,10 +26,6 @@ export default mongoose.model(
 			default: Date.now(),
 		},
 		createdBy: String,
-<<<<<<< HEAD
 		metaDataId: String,
-=======
-		metaDataId: String
->>>>>>> 2518239809748b41619eb93d361a1ebe24b6f1d9
 	}),
 );
