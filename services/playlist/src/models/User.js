817398bbcfb1a66a15c84ea0977162ad603e5719
noelimmi <noelimmanuel1998@gmail.com>
2020-01-27 23:27:16 +0530

    updated playlist model

--- services/playlist/src/models/User.js
+++ services/playlist/src/models/User.js
@@ -22,27 +22,34 @@ export default mongoose.model(
 			type: Date,
 			default: Date.now,
 		},
-		myPlaylists: [{
-			_id: String,
-			name: String,
-			playlistId: String,
-			thumbnail: {
-				type: String,
-				default: "https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg"
+		myPlaylists: [
+			{
+				_id: String,
+				name: String,
+				playlistId: String,
+				thumbnail: {
+					type: String,
+					default:
+						"https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg",
+				},
+				totalSongs: {
+					type: Number,
+					default: 0,
+				},
 			},
-			totalSongs: {
-				type: Number,
-				default: 0
-			}
-		}, ],
-		playlistCollections: [{
-			name: String,
-			playlistId: String,
-		}, ],
-		likedPlaylists: [{
-			name: String,
-			playlistId: String,
-		}, ],
+		],
+		playlistCollections: [
+			{
+				name: String,
+				playlistId: String,
+			},
+		],
+		likedPlaylists: [
+			{
+				name: String,
+				playlistId: String,
+			},
+		],
 		recentlyPlayedSongs: {
 			type: Array,
 			default: [],
