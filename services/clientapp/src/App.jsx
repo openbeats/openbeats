0324f60d559f35059d9337183540ecebf07a28f0
ThayalanGR <grthayalan18@gmail.com>
2020-02-02 01:28:50 +0530

    clientapp ytdl integration test build

--- services/clientapp/src/App.jsx
+++ services/clientapp/src/App.jsx
@@ -3,6 +3,12 @@ import { Route, Switch } from "react-router";
 import { Auth, Main } from "./containers";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import ytdl from 'ytdl-core';
+
+ytdl.getInfo("JGwWNGJdvx8", function (err, data) {
+  if (err) console.log(err)
+  else console.log(data)
+})
 
 class App extends Component {
   render() {
