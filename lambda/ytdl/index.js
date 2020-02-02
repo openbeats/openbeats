fd6e803d1f649dd8f40a6a926e5e15cd195cfbdd
ThayalanGR <grthayalan18@gmail.com>
2020-02-02 12:04:49 +0530

    ytdl lambda integration done

---
+++ lambda/ytdl/index.js
@@ -0,0 +1,22 @@
+const ytdl = require("ytdl-core");
+
+exports.handler = async (event) => {
+    // TODO implement
+    const vid = event.queryStringParameters && event.queryStringParameters.vid ? event.queryStringParameters.vid : null;
+    if (!vid) {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify("vid not found"),
+        };
+        return response;
+    }
+
+    console.log(vid)
+    const info = await ytdl.getInfo(vid)
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(info),
+    };
+    return response;
+
+};
--- services/server/src/index.js
+++ services/server/src/index.js
@@ -6,7 +6,7 @@ import {
 	copycat
 } from "./core";
 import ytdl from "ytdl-core";
-import HttpsProxyAgent from "https-proxy-agent";
+import fetch from 'node-fetch';
 
 
 // import dbconfig from "./config/db";
@@ -25,15 +25,7 @@ app.get("/", (req, res) => {
 app.get("/opencc/:id", async (req, res) => {
 	const videoID = req.params.id;
 	try {
-		const proxy = "http://lum-customer-hl_b2084710-zone-static_res-route_err-pass_dyn-country-in:5olhwmb9fyab@zproxy.lum-superproxy.io:22225";
-		// const proxy = "http://101.109.255.246:52279";
-		// const proxy = "https://api.proxyorbit.com/v1/?token=J0XAus0eRldTAZ17q0RF9QkxFKsTZoaU340Jz1omYO4&youtube=true";
-		const agent = new HttpsProxyAgent(proxy);
-		const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoID}`, {
-			requestOptions: {
-				agent: agent
-			}
-		});
+		const data = await (await fetch(`https://jkj2ip878k.execute-api.us-east-1.amazonaws.com/default/ytdl?vid=${videoID}`)).json();
 		let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
 		if (!audioFormats[0].contentLength) {
 			audioFormats = ytdl.filterFormats(info.formats, "audioandvideo");
