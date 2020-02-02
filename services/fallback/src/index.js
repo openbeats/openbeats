e5b31cc424105e42b8419a2c81f579f4c4030b99
ThayalanGR <grthayalan18@gmail.com>
2020-02-02 13:04:21 +0530

    lambda integration in server, fallback and downcc

--- services/fallback/src/index.js
+++ services/fallback/src/index.js
@@ -2,7 +2,7 @@ import middleware from "./config/middleware";
 import express from "express";
 import ytdl from "ytdl-core";
 import http from "https";
-import HttpsProxyAgent from "https-proxy-agent";
+import fetch from "node-fetch";
 // import dbconfig from "./config/db";
 // dbconfig();
 const PORT = process.env.PORT || 2000;
@@ -13,16 +13,8 @@ middleware(app);
 app.get("/:id", async (req, res) => {
 	const videoID = req.params.id;
 	try {
+		const info = await (await fetch(`https://jkj2ip878k.execute-api.us-east-1.amazonaws.com/default/ytdl?vid=${videoID}`)).json();
 
-		const proxy = "http://lum-customer-hl_b2084710-zone-static_res-route_err-pass_dyn-country-in:5olhwmb9fyab@zproxy.lum-superproxy.io:22225";
-		// const proxy = "http://101.109.255.246:52279";
-		// const proxy = "https://api.proxyorbit.com/v1/?token=J0XAus0eRldTAZ17q0RF9QkxFKsTZoaU340Jz1omYO4&youtube=true";
-		const agent = new HttpsProxyAgent(proxy);
-		const info = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoID}`, {
-			requestOptions: {
-				agent: agent
-			}
-		});
 		let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
 		if (!audioFormats[0].contentLength) {
 			audioFormats = ytdl.filterFormats(info.formats, "audioandvideo");
