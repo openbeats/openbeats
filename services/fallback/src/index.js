d54cd468f8572cde889d129995eaf52efc628a0b
noelimmi <noelimmanuel1998@gmail.com>
2020-02-05 22:40:23 +0530

    initial commit for english top chart

--- services/fallback/src/index.js
+++ services/fallback/src/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import ytdl from "ytdl-core";
 import http from "https";
 import fetch from "node-fetch";
+import ffmpeg from "fluent-ffmpeg";
+import { path } from "@ffmpeg-installer/ffmpeg";
 // import dbconfig from "./config/db";
 // dbconfig();
 const PORT = process.env.PORT || 2000;
@@ -13,33 +15,45 @@ middleware(app);
 app.get("/:id", async (req, res) => {
 	const videoID = req.params.id;
 	try {
-		const info = await (await fetch(`https://jkj2ip878k.execute-api.us-east-1.amazonaws.com/default/ytdl?vid=${videoID}`)).json();
+		const info = await (
+			await fetch(
+				`https://jkj2ip878k.execute-api.us-east-1.amazonaws.com/default/ytdl?vid=${videoID}`,
+			)
+		).json();
 
 		let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
 		if (!audioFormats[0].contentLength) {
 			audioFormats = ytdl.filterFormats(info.formats, "audioandvideo");
 		}
 		let sourceUrl = audioFormats[0].url;
-		const range = req.headers.range;
-		//when seeked range comes in header
-		if (range) {
-			http.get(
-				sourceUrl, {
-					headers: {
-						Range: req.headers.range,
-					},
-				},
-				function (response) {
-					res.writeHead(206, response.headers);
-					response.pipe(res);
-				},
-			);
-		} else {
-			http.get(sourceUrl, function (response) {
-				res.writeHead(200, response.headers);
-				response.pipe(res);
-			});
-		}
+		// const range = req.headers.range;
+		// //when seeked range comes in header
+		// if (range) {
+		// 	http.get(
+		// 		sourceUrl,
+		// 		{
+		// 			headers: {
+		// 				Range: req.headers.range,
+		// 			},
+		// 		},
+		// 		function(response) {
+		// 			res.writeHead(206, response.headers);
+		// 			response.pipe(res);
+		// 		},
+		// 	);
+		// } else {
+		// 	http.get(sourceUrl, function(response) {
+		// 		res.writeHead(200, response.headers);
+		// 		response.pipe(res);
+		// 	});
+		// }
+		ffmpeg({
+			source: sourceUrl,
+		}).setFfmpegPath(path);
+		proc.saveToFile(mp3, (stdout, stderr) => {
+			if (stderr) return console.log(stderr);
+			return console.log("Done");
+		});
 	} catch (error) {
 		console.error(error);
 		res.status(404).send({
