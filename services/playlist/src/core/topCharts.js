e6f500813383877df79a4fcbf7d4ec42691a5df9
ThayalanGR <grthayalan18@gmail.com>
2020-02-08 23:29:34 +0530

    topcharts promimse resolve bug fixed

--- services/playlist/src/core/topCharts.js
+++ services/playlist/src/core/topCharts.js
@@ -47,64 +47,29 @@ export const fetchTopCharts = async () => {
 	}
 };
 
-export const arrangeTopCharts = async () => {
-	console.log("arrange Top Charts Cron started");
-	const fetchlist = [
-		"bangla-top-10",
-		"kannada-top-20",
-		"punjabi-top-10",
-		"tamil-top-20",
-		"telugu-top-20",
-		"malayalam-top-20",
-		"marathi-top-20",
-		"mirchi-top-20",
-	];
+export const arrangeTopCharts = async (chartName) => {
+	console.log("arrange Top Charts Cron started", chartName);
 	try {
-		for (let chartName of fetchlist) {
-			let name = chartName.replace(/-/g, " ");
-			if (name === "mirchi top 20") {
-				name = "hindi top 20"
-			}
-			const chart = await TopChart.findOne({
-				name,
-			});
-			chart.songs.sort((x, y) => {
-				if (x.rank < y.rank) {
-					return -1;
-				} else {
-					return 1;
-				}
-			});
-			chart.totalSongs = chart.songs.length;
-			await chart.save();
+		let name = chartName.replace(/-/g, " ");
+
+		if (name === "mirchi top 20") {
+			name = "hindi top 20"
 		}
+
+		const chart = await TopChart.findOne({
+			name,
+		});
+
+		chart.songs.sort((x, y) => {
+			if (x.rank < y.rank) {
+				return -1;
+			} else {
+				return 1;
+			}
+		});
+		chart.totalSongs = chart.songs.length;
+		await chart.save();
 	} catch (error) {
 		console.error(error.message);
 	}
 };
-
-// export const fetchEnglishCharts = async () => {
-// 	try {
-// 		const language = "English";
-// 		const chart = await TopChart.findOne({
-// 			name: "Top 20 English",
-// 		});
-// 		if (!chart) {
-// 			chart = new TopChart({
-// 				name: "Top 20 English",
-// 				language,
-// 				songs: [],
-// 				createdBy: "Openbeats",
-// 			});
-// 			await chart.save();
-// 		} else {
-// 			chart.songs = [];
-// 			chart.updatedAt = Date.now();
-// 			await chart.save();
-// 		}
-// 		const chartId = chart.id;
-// 		await updateEnglishTopCharts(chartId);
-// 	} catch (error) {
-// 		console.error(error.message);
-// 	}
-// };
