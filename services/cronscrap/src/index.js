704d7ab182f892f1fe8a541745b91f167245eced
noelimmi <noelimmanuel1998@gmail.com>
2020-02-26 23:15:35 +0530

    maded changes for PR of recently added and cron services

--- services/cronscrap/src/index.js
+++ services/cronscrap/src/index.js
@@ -5,6 +5,7 @@ import croninit from "./routes/croninit";
 import topcharts from "./routes/topcharts";
 import cron from "node-cron";
 import fetchRetry from "./core/refetch";
+import config from "config";
 
 
 dbconfig();
@@ -19,7 +20,13 @@ app.use("/topcharts", topcharts);
 
 
 cron.schedule("0 0 * * 0", async () => {
-  const result = await (await fetchRetry("http://localhost:2000/cron/init_topcharts")).json();
+  let scrapUrl = "";
+  if (config.get("isDev")) {
+    scrapUrl = config.get("scrapurl").dev;
+  } else {
+    scrapUrl = config.get("scrapurl").production;
+  }
+  const result = await (await fetchRetry(`${scrapUrl}/cron/init_topcharts`)).json();
   if (result.status) {
     console.log(`Successfully topcharts cron started.`);
   }
