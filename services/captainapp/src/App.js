9daa8d64e69ba75168038aecd6505c4e1fc056a1
ThayalanGR <grthayalan18@gmail.com>
2020-02-29 12:27:31 +0530

    captain app initiation commit

--- services/captainapp/src/App.js
+++ services/captainapp/src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Auth, Home } from './components';
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      userName: '',
+      email: ''
+    }
+
+    this.setAuthDetails = this.setAuthDetails.bind(this);
+  }
+
+  setAuthDetails(details) {
+    this.setState({
+      isAuthenticated: details.status,
+      userName: details.userName,
+      email: details.mail
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Openbeats Captain Portal</h2>
-        </div>
+      <div className="core-wrapper">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={() => {
+              if (this.state.isAuthenticated)
+                return <Home />
+              else
+                return <Redirect to="/auth" />
+            }} />
+            <Route path="/auth" component={() => {
+              if (!this.state.isAuthenticated) {
+                return <Auth
+                  setAuthDetails={this.setAuthDetails}
+                />
+              } else
+                return <Redirect to="/" />
+            }} />
+          </Switch>
+        </Router>
       </div>
     );
   }
