c806cf53bf9d88917f9fc358b517b25a5472325c
ThayalanGR <grthayalan18@gmail.com>
2020-03-10 23:44:31 +0530

    captain app leftnav navigation items and router configurations done

--- services/captainapp/src/components/LeftNav.jsx
+++ services/captainapp/src/components/LeftNav.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from "react-router-dom";
 import { logo } from "../assets/images";
 import "../assets/styles/leftnav.css";
 
@@ -8,20 +9,27 @@ class LeftNav extends Component {
         return (
             <div className="leftnav-wrapper">
                 <ul className="leftnav-menu">
-                    <li className="leftnav-item">
-                        <img className="leftnav-logo-img" src={logo} alt="" srcset="" />
-                    </li>
-                    <li className="leftnav-item">
-                        Albums
-                    </li>
-                    <li className="leftnav-item">
-                        Albums
-                    </li>
-                    <li className="leftnav-item">
-                        Albums
-                    </li>
-                    <li className="leftnav-item">
-                        Albums
+                    <a href="/" className="leftnav-item">
+                        <img className="leftnav-logo-img" src={logo} alt="" srcSet="" />
+                    </a>
+                    <Link to="/" className="leftnav-item" title="Home - view insights and more">
+                        <i className="far fa-home"></i>
+                        <span>Home</span>
+                    </Link>
+                    <Link to="/albums" className="leftnav-item" title="Create, View and Update Albums">
+                        <i className="fas fa-album-collection"></i>
+                        <span>Albums</span>
+                    </Link>
+                    <Link to="/artists" className="leftnav-item" title="Add, Update and delete Artists">
+                        <i className="fas fa-user-music"></i>
+                        <span>Artists</span>
+                    </Link>
+                    <Link to="languages" className="leftnav-item" title="Add, Update and Delete Languages">
+                        <i className="fas fa-language"></i>
+                        <span>Tounge</span>
+                    </Link>
+                    <li className="leftnav-item" title="ThayalanGR - Profile settings and Logout">
+                        <img className="leftnav-profile-img" src={"https://lh3.googleusercontent.com/-OTO38IoJfKM/AAAAAAAAAAI/AAAAAAAAAAA/AKF05nA74VsD5AoXBma8QTjD06srMgrI1A/photo.jpg?sz=46"} alt="" srcSet="" />
                     </li>
                 </ul>
             </div>
--- services/captainapp/src/components/index.js
+++ services/captainapp/src/components/index.js
@@ -3,11 +3,13 @@ import Home from './Home';
 import LeftNav from './LeftNav';
 import Albums from './Albums';
 import Artists from './Artists';
+import Languages from './Languages';
 
 export {
     Auth,
     Home,
     LeftNav,
     Albums,
-    Artists
+    Artists,
+    Languages
 }
