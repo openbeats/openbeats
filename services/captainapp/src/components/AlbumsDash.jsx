009aa0b28f7dd8ce460007c2ef3395a2a65f7ede
ThayalanGR <grthayalan18@gmail.com>
2020-03-29 21:41:19 +0530

    captain app album dashyard partial implementation done

--- services/captainapp/src/components/AlbumsDash.jsx
+++ services/captainapp/src/components/AlbumsDash.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import "../assets/styles/albumsdash.css";
+import Chips from "react-chips";
 
 export default class AlbumsDash extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            artistChips: [],
+            searchChips: []
+        }
+    }
+
+    artistOnChange = (chips) => {
+        this.setState({ artistChips: chips })
+
+    }
+    searchOnChange = (chips) => {
+        this.setState({ searchChips: chips })
+
+    }
+
+    fetchArtistSuggestion = async (value) => {
+        console.log(value, "artist suggestion")
+        return true;
+    }
+
+
     render() {
         return (
             <div className="albumsdash-wrapper">
@@ -12,6 +36,37 @@ export default class AlbumsDash extends Component {
                     <div className="d-flex">
                         <div className="create-album-link font-weight-bold mr-3 cursor-pointer" >save</div>
                         <div className="create-album-link bg-danger font-weight-bold cursor-pointer" >cancel</div>
+                    </div>
+                </div>
+                <div className="albumdash-container">
+                    <div className="albumdash-container-left-pan">
+                        <div className="albumdash-album-content-holder p-4">
+                            <div className="albumdash-name-holder">
+                                <div className="font-weight-bold">
+                                    Album Name
+                                </div>
+                                <div className="mb-1">
+                                    (max 30 Characters)
+                                </div>
+                                <input className="rounded w-100" placeholder="This is Yuvan Shankar Raja" type="text" name="" id="" />
+                            </div>
+                            <div className="albumdash-artist-tags-holder mt-2">
+                                <div className="artist-tags-title font-weight-bold">Artist Tags</div>
+                                <div className="artist-tags-title">Artist Tags</div>
+                                <Chips
+
+                                    value={this.state.artistChips}
+                                    onChange={this.artistOnChange}
+                                    suggestions={["Your", "Data", "Here"]}
+                                    fetchSuggestions={(value) => { this.fetchArtistSuggestion(value) }}
+                                // theme={}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="albumdash-container-right-pan">
+                        <div className="albumdash-search-holder"></div>
+                        <div className="albumdash-song-bucket-holder"></div>
                     </div>
                 </div>
             </div>
