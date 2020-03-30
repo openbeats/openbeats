f71214e7d79838cc45df3fa7f88b228e02d0219e
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-03-31 05:06:36 +0530

    Implemented changes for songs coll album & Recent

--- services/playlist/src/models/Artist.js
+++ services/playlist/src/models/Artist.js
@@ -1,26 +1,40 @@
 import mongoose from "mongoose";
 
 const artistSchema = new mongoose.Schema({
-  name: String,
-  thumbnail: {
-    type: String,
-    default: "https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg"
-  },
-  albumTags: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Album",
-    default: []
-  }]
-})
+	name: String,
+	thumbnail: {
+		type: String,
+		default:
+			"https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg",
+	},
+	releasedAlbums: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Album",
+			default: [],
+		},
+	],
+	featuringAlbums: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Album",
+			default: [],
+		},
+	],
+});
 
-artistSchema.methods.addAlbumTag = async function (albumId) {
-  if (!this.albumTags.includes(albumId)) {
-    this.albumTags.push(albumId);
-    this.save();
-  }
+artistSchema.methods.addAsReleasedAlbums = async function(albumId) {
+	if (!this.releasedAlbums.includes(albumId)) {
+		this.releasedAlbums.push(albumId);
+		await this.save();
+	}
 };
 
-export default mongoose.model(
-  "Artist",
-  artistSchema
-);
+artistSchema.methods.addAsFeaturingAlbums = async function(albumId) {
+	if (!this.featuringAlbums.includes(albumId)) {
+		this.featuringAlbums.push(albumId);
+		await this.save();
+	}
+};
+
+export default mongoose.model("Artist", artistSchema);
