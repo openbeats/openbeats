3074dd637a7c1918a498b8231458bab92a9fdbd3
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-04-05 18:14:39 +0530

    Refractored album,artist,searchTag model and routes

--- services/playlist/src/models/Artist.js
+++ services/playlist/src/models/Artist.js
@@ -4,37 +4,8 @@ const artistSchema = new mongoose.Schema({
 	name: String,
 	thumbnail: {
 		type: String,
-		default:
-			"https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg",
-	},
-	releasedAlbums: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Album",
-			default: [],
-		},
-	],
-	featuringAlbums: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Album",
-			default: [],
-		},
-	],
+		default: "https://openbeats.live/static/media/dummy_music_holder.a3d0de2e.jpg",
+	}
 });
-
-artistSchema.methods.addAsReleasedAlbums = async function(albumId) {
-	if (!this.releasedAlbums.includes(albumId)) {
-		this.releasedAlbums.push(albumId);
-		await this.save();
-	}
-};
-
-artistSchema.methods.addAsFeaturingAlbums = async function(albumId) {
-	if (!this.featuringAlbums.includes(albumId)) {
-		this.featuringAlbums.push(albumId);
-		await this.save();
-	}
-};
 
 export default mongoose.model("Artist", artistSchema);
