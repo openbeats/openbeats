cc0e9c5010f55bf400841e2cdd6a3df2c5d208c0
ThayalanGR <grthayalan18@gmail.com>
2020-04-10 19:18:10 +0530

    artist component backend wireup done

--- services/clientapp/src/components/Artists.jsx
+++ services/clientapp/src/components/Artists.jsx
@@ -4,21 +4,59 @@ import "../css/artists.css";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import { musicDummy } from '../images';
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import { variables } from '../config';
 
 class Artists extends Component {
-    componentDidMount() {
-        this.props.setCurrentAction("Artists")
+    constructor(props) {
+        super(props);
+        this.initialState = {
+            artists: [],
+            isLoading: true
+        }
+        this.state = { ...this.initialState };
     }
+
+    componentDidMount() {
+        this.props.setCurrentAction("Artists");
+        this.fetchArtistHandler();
+
+    }
+
+    fetchArtistHandler = async () => {
+        const artistsFetchUrl = `${variables.baseUrl}/playlist/artist/all?page=1&limit=10000`;
+        const data = (await axios.get(artistsFetchUrl)).data;
+        console.log(data);
+        if (data.status) {
+            this.setState({ isLoading: false, artists: data.data.result });
+        } else {
+            this.props.notify(data.data.toString());
+        }
+    }
+
+    aritstClickHandler = async (id) => {
+        this.props.push("/artists/" + id);
+    }
+
+    componentWillUnmount() {
+        this.setState({ ...this.initialState });
+    }
+
     render() {
-        return (
+        return this.state.isLoading ?
+            <div className="width-100 height-100 d-flex align-items-center justify-content-center">
+                <Loader type="ThreeDots" color="#F32C2C" height={80} width={80} />
+            </div> :
             <div className="artists-wrapper">
-                <div className="artist-display-holder cursor-pointer">
-                    <div className="artist-rounded-circle-holder" style={{ backgroundImage: `url(''), url(${musicDummy})` }}></div>
-                    <div className="artist-name">A R Rahman</div>
-                    <div className="artist-description">Artist</div>
-                </div>
+                {this.state.artists.map((item, key) => (
+                    <div className="artist-display-holder cursor-pointer" onClick={() => this.aritstClickHandler(item._id)} key={key}>
+                        <div className="artist-rounded-circle-holder" style={{ backgroundImage: `url('${item.thumbnail}'), url(${musicDummy})` }}></div>
+                        <div className="artist-name">{item.name}</div>
+                        <div className="artist-description">Artist</div>
+                    </div>
+                ))}
             </div>
-        )
     }
 }
 
