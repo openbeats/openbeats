e06da2f398e04a046f2fbe6053477f322646d404
ThayalanGR <grthayalan18@gmail.com>
2020-04-18 14:06:35 +0530

    playlist display actions deep restructuring done

--- services/clientapp/src/components/AlbumHolder.jsx
+++ services/clientapp/src/components/AlbumHolder.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import "../css/albumholder.css";
 import { musicDummy } from '../images';
+import { push } from 'connected-react-router';
+import { toastActions, playlistManipulatorActions, nowPlayingActions } from '../actions';
+import { connect } from 'react-redux';
 
-export default class AlbumHolder extends Component {
-    checkIfAlbumIsInCollectionHandler = () => {
+class AlbumHolder extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
     }
 
     addOrRemoveAlbumFromCollectionHandler = () => {
@@ -14,14 +21,27 @@ export default class AlbumHolder extends Component {
         }
     }
 
+    albumViewCallBack = async (id) => {
+        this.props.push(`/playlist/${this.props.type}/${id}`);
+    }
+
+    albumPlayCallBack = async (id) => {
+        this.props.push(`/playlist/${this.props.type}/${id}?autoplay=true`);
+    }
+
+    albumAddToCurrentQueueCallBack = async (id) => {
+        this.props.addSongsToQueue(id, this.props.type);
+    }
+
+
     render() {
         return (
             <div className="album-holder-wrapper" style={{ backgroundImage: `url(${this.props.albumThumbnail}), url(${musicDummy})` }}>
                 {this.props.addOrRemoveAlbumFromCollectionHandler && this.props.isAuthenticated && <i className={`fas fa-heart album-add-to-collection-icon ${this.props.isAlbumIsInCollection ? "master-color" : ''}`} title={this.props.isAlbumIsInCollection ? "Remove from My Collection" : "Add to My Collection"} onClick={this.addOrRemoveAlbumFromCollectionHandler}></i>}
                 <div className="album-holder-play-icon-visible-on-hover">
-                    <i className="far fa-eye" title="View this Album" onClick={() => this.props.albumViewCallBack(this.props.albumId)}></i>
-                    <i className="fas fa-play" title="Reset Current Queue and Play this Album" onClick={() => this.props.albumPlayCallBack(this.props.albumId)}></i>
-                    <i className="fas fa-plus-square" title="Add to The current Queue" onClick={() => this.props.albumAddToCurrentQueueCallBack(this.props.albumId)}></i>
+                    <i className="far fa-eye" title="View this Album" onClick={() => this.albumViewCallBack(this.props.albumId)}></i>
+                    <i className="fas fa-play" title="Reset Current Queue and Play this Album" onClick={() => this.albumPlayCallBack(this.props.albumId)}></i>
+                    <i className="fas fa-plus-square" title="Add to The current Queue" onClick={() => this.albumAddToCurrentQueueCallBack(this.props.albumId)}></i>
                 </div>
                 <div className="album-holder-songs-total-holder">{this.props.albumTotalSongs}</div>
                 <div className="album-holder-album-desc">
@@ -32,4 +52,35 @@ export default class AlbumHolder extends Component {
             </div>
         )
     }
+
 }
+
+const mapStateToProps = (state) => {
+    return {
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        push: (path) => {
+            dispatch(push(path))
+        },
+        notify: (message) => {
+            toastActions.showMessage(message)
+        },
+        featureNotify: () => {
+            toastActions.featureNotify()
+        },
+        addSongsToQueue: async (pId, type = 'album') => {
+            const data = await playlistManipulatorActions.fetchAlbumPlaylist(pId);
+            if (data && data.status && data.data.songs.length) {
+                nowPlayingActions.addSongsToQueue(data.data.songs);
+            } else {
+                toastActions.showMessage("Playlist you tried to add to the queue.. seems to be empty!")
+            }
+        },
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumHolder);
