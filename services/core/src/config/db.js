0a063c5b92b639e695725fe2a482766299dcb29f
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-04-23 21:33:20 +0530

    Changed DB URL to mongodbatlas

--- services/core/src/config/db.js
+++ services/core/src/config/db.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose";
 import config from "config";
 
 export default async () => {
-	let mongoUrl = "";
-	if (config.get("isDev")) {
-		mongoUrl = config.get("mongoURI_DEV");
-	} else {
-		mongoUrl = `mongodb://${process.env.DB}/obs-db`;
-	}
+	const mongoUrl = config.get("mongoURI_DEV");
 	await mongoose
 		.connect(mongoUrl, {
 			useNewUrlParser: true,
