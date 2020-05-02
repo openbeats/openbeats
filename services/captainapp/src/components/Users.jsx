d07892fd407a9714eb38cb7a3330d79e709db592
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-05-02 13:55:58 +0530

    Done User Acess Implementation

--- services/captainapp/src/components/Users.jsx
+++ services/captainapp/src/components/Users.jsx
@@ -1,19 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Fragment } from "react";
 import axios from "axios";
 import Loader from "react-loader-spinner";
 import "../assets/styles/users.css";
 import { variables } from "../config";
+import { toast } from "react-toastify";
+import { connect } from "react-redux";
 
-const Users = () => {
+const Users = ({ adminDetails }) => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [usersCollection, setUsersCollection] = useState([]);
 	useEffect(() => {
 		fetchUsers();
 	}, []);
+	const onChangeAdmin = async (accessLevel, userId) => {
+		try {
+			const body = {};
+			body.userId = userId;
+			body.adminStatus = false;
+			body.accessLevel = null;
+			if (accessLevel !== "None") {
+				body.adminStatus = true;
+				body.accessLevel = accessLevel;
+			}
+			const adminAccess = (await axios.put(`${variables.baseUrl}/auth/admin`, body)).data;
+			if (adminAccess.status) {
+				await fetchUsers();
+			} else {
+				throw new Error(adminAccess.data);
+			}
+		} catch (error) {
+			toast.error(error.toString());
+		}
+	};
 	const fetchUsers = async () => {
-		const users = (await axios.get(`${variables.baseUrl}/auth/admin`)).data;
-		if (users.status) {
-			setUsersCollection(users.data.result);
+		try {
+			const users = (await axios.get(`${variables.baseUrl}/auth/admin?page=1&limit=1000`)).data;
+			if (users.status) {
+				setUsersCollection(users.data.result);
+			} else {
+				throw new Error(users.data);
+			}
+		} catch (error) {
+			toast.error(error.toString());
 		}
 		isLoading && setIsLoading(false);
 	};
@@ -21,7 +49,7 @@ const Users = () => {
 		<div className="users-wrapper">
 			<div className="users-header">
 				<div className="user-indicator d-flex align-items-center font-weight-bold base-color h5-responsive">
-					<i className="fas fa-angle-right mr-1 right-angel"></i>Artists
+					<i className="fas fa-angle-right mr-1 right-angel"></i>Users
 				</div>
 				<div className="user-search-input">
 					<input className="input input-sm red-border" type="text" placeholder="Search Albums, Artists, Languages..." aria-label="Search" />
@@ -30,7 +58,7 @@ const Users = () => {
 			</div>
 			<div className="users-container">
 				{isLoading ? (
-					<div className="width-100 height-100 d-flex align-items-center justify-content-center">
+					<div className="loader-center">
 						<Loader type="ThreeDots" color="#F32C2C" height={80} width={80} />
 					</div>
 				) : (
@@ -38,7 +66,23 @@ const Users = () => {
 						<div className="user-display-holder cursor-pointer" key={index}>
 							<div className="user-rounded-circle-holder" style={{ backgroundImage: `url(${item.avatar})` }}></div>
 							<div className="user-name">{item.name}</div>
+							<div className="user-description">{`${item.email}`}</div>
 							<div className="user-description">{`Joined At ${new Date(item.date).toDateString()}`}</div>
+							{adminDetails.accessLevel === 3 && (
+								<Fragment>
+									<div className="user-name">Admin Access</div>
+									<select
+										value={!item.admin.accessLevel ? "None" : item.admin.accessLevel}
+										onChange={e => onChangeAdmin(e.target.value, item._id)}
+										className="cars"
+										name="cars">
+										<option value="None">Non-admin</option>
+										<option value="1">Access Level 1</option>
+										<option value="2">Access Level 2</option>
+										<option value="3">Access Level 3</option>
+									</select>
+								</Fragment>
+							)}
 						</div>
 					))
 				)}
@@ -47,4 +91,6 @@ const Users = () => {
 	);
 };
 
-export default Users;
+const mapStateToProps = state => ({ adminDetails: state.auth.adminDetails });
+
+export default connect(mapStateToProps, null)(Users);
