33f1d81cfa9980f8568106424a798d4ee62cc23d
ThayalanGR <grthayalan18@gmail.com>
2020-05-24 21:35:30 +0530

    clientapp pagination scrollfetch implementation for albums, artists, languages and emotions implemented

--- services/clientapp/src/components/Artists.jsx
+++ services/clientapp/src/components/Artists.jsx
@@ -13,7 +13,12 @@ class Artists extends Component {
         super(props);
         this.initialState = {
             artists: [],
-            isLoading: true
+            isLoading: true,
+            next: true,
+            previous: false,
+            page: 1,
+            limit: 20,
+            isScrollFetchInProcess: false,
         }
         this.state = { ...this.initialState };
     }
@@ -21,16 +26,38 @@ class Artists extends Component {
     componentDidMount() {
         this.props.setCurrentAction("Artists");
         this.fetchArtistHandler();
+        this.initiateScrollFetcher();
+    }
 
+    initiateScrollFetcher() {
+        const mainBodyRef = document.getElementById("main-body");
+        mainBodyRef.addEventListener('scroll', this.scrollFetch);
+    }
+
+    scrollFetch = (e) => {
+        const totalHeight = e.target.scrollHeight - e.target.offsetHeight;
+        if (e.target.scrollTop === totalHeight) {
+            this.fetchArtistHandler();
+        }
     }
 
     fetchArtistHandler = async () => {
-        const artistsFetchUrl = `${variables.baseUrl}/playlist/artist/all?page=1&limit=10000`;
-        const data = (await axios.get(artistsFetchUrl)).data;
-        if (data.status) {
-            this.setState({ isLoading: false, artists: data.data.result });
-        } else {
-            this.props.notify(data.data.toString());
+        if (this.state.next && !this.state.isScrollFetchInProcess) {
+            this.setState({ isScrollFetchInProcess: true })
+            const artistsFetchUrl = `${variables.baseUrl}/playlist/artist/all?page=${this.state.page}&limit=${this.state.limit}`;
+            const data = (await axios.get(artistsFetchUrl)).data;
+            if (data.status) {
+                this.setState({
+                    isLoading: false,
+                    artists: [...this.state.artists, ...data.data.result],
+                    page: data.data.next ? this.state.page + 1 : this.state.page,
+                    next: data.data.next ? true : false,
+                    previous: data.data.previous ? true : false,
+                    isScrollFetchInProcess: false
+                });
+            } else {
+                this.props.notify(data.data.toString());
+            }
         }
     }
 
@@ -38,23 +65,31 @@ class Artists extends Component {
 
     componentWillUnmount() {
         this.setState({ ...this.initialState });
+        const mainBodyRef = document.getElementById("main-body");
+        mainBodyRef.removeEventListener("scroll", this.scrollFetch);
     }
 
     render() {
         return this.state.isLoading ?
             <div className="width-100 height-100 d-flex align-items-center justify-content-center">
                 <Loader type="ThreeDots" color="#F32C2C" height={80} width={80} />
-            </div> :
-            <div className="artists-wrapper">
-                {this.state.artists.map((item, key) => (
-                    <ArtistHolder
-                        key={key}
-                        id={item._id}
-                        name={item.name}
-                        thumbnail={item.thumbnail}
-                    />
-                ))}
-            </div>
+            </div> : (
+                <>
+                    <div className="artists-wrapper">
+                        {this.state.artists.map((item, key) => (
+                            <ArtistHolder
+                                key={key}
+                                id={item._id}
+                                name={item.name}
+                                thumbnail={item.thumbnail}
+                            />
+                        ))}
+                    </div>
+                    {this.state.isScrollFetchInProcess && <div className="mt-2 width-100 d-flex align-items-center justify-content-center">
+                        <Loader color="#F32C2C" type="TailSpin" height={30} width={30} />
+                    </div>}
+                </>
+            )
     }
 }
 
