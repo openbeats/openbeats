aa557a0a9f4c0052d8baee389d4d4970bf1a7dd0
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-05-24 18:24:02 +0530

    Added delete user route in auth service

--- services/auth/src/config/paginationMiddleware.js
+++ services/auth/src/config/paginationMiddleware.js
@@ -3,27 +3,30 @@ export default (model, query = {}, options = {}, populateQuery = []) => {
 		try {
 			const page = parseInt(req.query.page);
 			const limit = parseInt(req.query.limit);
-			const type = req.query.type; // popular/latest/all
 			if (!page || !limit) {
 				throw new Error("Please send page and limit as query params.");
 			}
 			const startIndex = (page - 1) * limit;
 			const endIndex = page * limit;
 			const data = {};
-			endIndex < (await model.countDocuments())
-				? (data.next = {
-						page: page + 1,
-						limit,
-				  })
-				: (data.next = null);
+			endIndex < (await model.countDocuments()) ?
+				(data.next = {
+					page: page + 1,
+					limit,
+				}) :
+				(data.next = null);
 
-			startIndex > 0
-				? (data.previous = {
-						page: page - 1,
-						limit,
-				  })
-				: (data.previous = null);
-			data.result = await model.find(query, options).populate(populateQuery).limit(limit).skip(startIndex);
+			startIndex > 0 ?
+				(data.previous = {
+					page: page - 1,
+					limit,
+				}) :
+				(data.previous = null);
+
+			const sortOrder = {
+				_id: -1,
+			};
+			data.result = await model.find(query, options).sort(sortOrder).populate(populateQuery).limit(limit).skip(startIndex);
 			if (!(data.result.length > 0)) data.result = [];
 			res.paginatedResults = data;
 			next();
