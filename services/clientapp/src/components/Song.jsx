7716f7f52616602125259144bdb15936f668f7ea
ThayalanGR <grthayalan18@gmail.com>
2020-05-24 21:35:30 +0530

    clientapp pagination scrollfetch implementation for albums, artists, languages and emotions implemented

--- services/clientapp/src/components/Song.jsx
+++ services/clientapp/src/components/Song.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "../assets/css/song.css";
-import { playerpause, playerplay, playerdownload, pQueueRed, playlistadd } from '../assets/images';
+import { playerpause, playerplay, playerdownload, pQueueRed, playlistadd, musicDummy } from '../assets/images';
 import Loader from 'react-loader-spinner';
 import { toastActions, playlistManipulatorActions, nowPlayingActions, playerActions } from '../actions';
 import { connect } from 'react-redux';
@@ -17,80 +17,74 @@ class Song extends Component {
     render() {
         return (
             <div className={`result-node ${this.props.currentPlaying && this.props.currentPlaying.videoId === this.props.item.videoId ? "highlight-active-result" : ""}`}>
-                <div className="result-node-thumb">
-                    <img src={this.props.item.thumbnail} alt="" />
+                <div className="result-node-thumb" style={{ backgroundImage: `url('${this.props.item.thumbnail}'), url(${musicDummy})` }}></div>
+                <div className="result-node-title">
+                    {this.props.item.title}
                 </div>
-                <div className="result-node-desc">
-                    <div className="result-node-title">
-                        {this.props.item.title}
-                    </div>
-                    <div className="result-node-attributes">
-                        <div className="result-node-duration">
-                            {this.props.item.duration}
-                        </div>
-                        <div className="result-node-actions">
-                            {this.props.currentPlaying && this.props.currentPlaying.videoId === this.props.item.videoId ?
-                                !this.props.isAudioBuffering ?
-                                    this.props.isMusicPlaying ?
-                                        <img onClick={async (e) => {
-                                            await this.props.playPauseToggle()
-                                        }} className="action-image-size play-icon-result cursor-pointer" src={playerpause} alt="" />
-                                        :
-                                        <img onClick={async (e) => {
-                                            await this.props.playPauseToggle()
-                                        }} className="action-image-size play-icon-result cursor-pointer" src={playerplay} alt="" />
-                                    :
-                                    <Loader
-                                        type="Rings"
-                                        color="#F32C2C"
-                                        height={30}
-                                        width={30}
-                                    />
+                <div className="result-node-duration">
+                    {this.props.item.duration}
+                </div>
+                <div className="result-node-actions">
+                    {this.props.currentPlaying && this.props.currentPlaying.videoId === this.props.item.videoId ?
+                        !this.props.isAudioBuffering ?
+                            this.props.isMusicPlaying ?
+                                <img onClick={async (e) => {
+                                    await this.props.playPauseToggle()
+                                }} className="action-image-size play-icon-result cursor-pointer" src={playerpause} alt="" />
                                 :
                                 <img onClick={async (e) => {
-                                    await this.props.updateCurrentPlaying(this.props.item, this.props.index)
+                                    await this.props.playPauseToggle()
                                 }} className="action-image-size play-icon-result cursor-pointer" src={playerplay} alt="" />
-                            }
+                            :
+                            <Loader
+                                type="Rings"
+                                color="#F32C2C"
+                                height={30}
+                                width={30}
+                            />
+                        :
+                        <img onClick={async (e) => {
+                            await this.props.updateCurrentPlaying(this.props.item, this.props.index)
+                        }} className="action-image-size play-icon-result cursor-pointer" src={playerplay} alt="" />
+                    }
 
 
-                            <div download
-                                onClick={async (e) => {
-                                    e.preventDefault();
-                                    await this.setState({ videoId: this.props.item.videoId });
-                                    if (await this.props.downloadSongHandler(this.props.item)) {
-                                        this.setState({ videoId: null })
-                                    }
-                                }}
-                                className="t-none cursor-pointer" >
-                                {this.state.videoId === this.props.item.videoId ?
-                                    <Loader
-                                        type="Oval"
-                                        color="#F32C2C"
-                                        height={20}
-                                        width={20}
-                                    />
-                                    :
-                                    <img className="action-image-size " src={playerdownload} alt="" />
-                                }
-                            </div>
-                            <img onClick={
-                                () => {
-                                    this.props.addSongsToQueue(this.props.item);
-                                }
-                            } className="action-image-size cursor-pointer queue-icon-result" title="Add to Queue" src={pQueueRed} alt="" />
-                            <img onClick={
-                                () => {
-                                    this.props.showAddPlaylistDialog(this.props.item)
-                                }
-                            } className="action-image-size cursor-pointer" title="Add to playlist" src={playlistadd} alt="" />
-                            {this.props.deleteSongFromUserPlaylist &&
-                                <i
-                                    title="Delete this song from this playlist!"
-                                    className="action-image-size cursor-pointer fas fa-trash-alt playlist-display-songs-icon"
-                                    onClick={() => this.props.deleteSongFromUserPlaylist(this.props.item)}></i>
+                    <div download
+                        onClick={async (e) => {
+                            e.preventDefault();
+                            await this.setState({ videoId: this.props.item.videoId });
+                            if (await this.props.downloadSongHandler(this.props.item)) {
+                                this.setState({ videoId: null })
                             }
-                        </div>
+                        }}
+                        className="t-none cursor-pointer" >
+                        {this.state.videoId === this.props.item.videoId ?
+                            <Loader
+                                type="Oval"
+                                color="#F32C2C"
+                                height={20}
+                                width={20}
+                            />
+                            :
+                            <img className="action-image-size " src={playerdownload} alt="" />
+                        }
                     </div>
+                    <img onClick={
+                        () => {
+                            this.props.addSongsToQueue(this.props.item);
+                        }
+                    } className="action-image-size cursor-pointer queue-icon-result" title="Add to Queue" src={pQueueRed} alt="" />
+                    <img onClick={
+                        () => {
+                            this.props.showAddPlaylistDialog(this.props.item)
+                        }
+                    } className="action-image-size cursor-pointer" title="Add to playlist" src={playlistadd} alt="" />
+                    {this.props.deleteSongFromUserPlaylist &&
+                        <i
+                            title="Delete this song from this playlist!"
+                            className="action-image-size cursor-pointer fas fa-trash-alt playlist-display-songs-icon"
+                            onClick={() => this.props.deleteSongFromUserPlaylist(this.props.item)}></i>
+                    }
                 </div>
             </div>
         )
