876fa68b7a0bcfe9722a9e575f9c92a6bf934697
ThayalanGR <grthayalan18@gmail.com>
2020-05-25 21:29:09 +0530

    clientapp aritst albums scroll fetch integration done

--- services/clientapp/src/components/Language.jsx
+++ services/clientapp/src/components/Language.jsx
@@ -22,7 +22,7 @@ class Language extends Component {
     ExploreMore = () => {
         return <div className="language-holder-wrapper album-explore-more cursor-pointer" onClick={() => this.props.push(this.props.exploreMoreUrl)}>
             <div className="explore-more-icon-holer">
-                <i class="fas fa-compass"></i>
+                <i className="fas fa-compass"></i>
             </div>
             <div className="explore-more-text-holer">
                 Explore More
