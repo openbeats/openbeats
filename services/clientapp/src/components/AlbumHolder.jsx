7baddb7cbb7159153820d456f4fe1c39ce0d7fa5
ThayalanGR <grthayalan18@gmail.com>
2020-05-25 21:05:45 +0530

    explore more integration and language based aggreagation done

--- services/clientapp/src/components/AlbumHolder.jsx
+++ services/clientapp/src/components/AlbumHolder.jsx
@@ -37,36 +37,50 @@ class AlbumHolder extends Component {
     }
 
 
-    render() {
-        return (
-            <div className="album-holder-wrapper" onClick={(e) => {
-                if (this.heartRef && this.heartRef.contains(e.target))
-                    return;
-                else if (this.playRef && this.playRef.contains(e.target))
-                    return
-                this.albumViewCallBack(this.props.albumId);
-            }} style={{ backgroundImage: `url(${this.props.albumThumbnail}), url(${musicDummy})` }}>
-                {this.props.addOrRemoveAlbumFromCollectionHandler &&
-                    this.props.isAuthenticated &&
-                    <i className={`fas fa-heart album-add-to-collection-icon ${this.props.isAlbumIsInCollection ? "master-color" : ''}`}
-                        title={this.props.isAlbumIsInCollection ? "Remove from My Collection" : "Add to My Collection"}
-                        onClick={this.addOrRemoveAlbumFromCollectionHandler}
-                        ref={d => this.heartRef = d}
-                    ></i>
-                }
-                <div className="album-holder-play-icon-visible-on-hover">
-                    <i className="far fa-eye" title="View this Album" onClick={() => this.albumViewCallBack(this.props.albumId)}></i>
-                    <i className="fas fa-play" title="Reset Current Queue and Play this Album" ref={d => this.playRef = d} onClick={() => this.albumPlayCallBack(this.props.albumId)}></i>
-                    <i className="fas fa-plus-square" title="Add to The current Queue" onClick={() => this.albumAddToCurrentQueueCallBack(this.props.albumId)}></i>
-                </div>
-                <div className="album-holder-songs-total-holder">{this.props.albumTotalSongs}</div>
-                <div className="album-holder-album-desc">
-                    <div className="album-holder-album-title">{this.props.albumName}</div>
-                    <div className="album-holder-album-date">{this.props.albumCreationDate}</div>
-                    <div className="album-holder-album-by">by {this.props.albumCreatedBy}</div>
-                </div>
+    Main = () => {
+        return <div className="album-holder-wrapper" onClick={(e) => {
+            if (this.heartRef && this.heartRef.contains(e.target))
+                return;
+            else if (this.playRef && this.playRef.contains(e.target))
+                return
+            this.albumViewCallBack(this.props.albumId);
+        }} style={{ backgroundImage: `url(${this.props.albumThumbnail}), url(${musicDummy})` }}>
+            {this.props.addOrRemoveAlbumFromCollectionHandler &&
+                this.props.isAuthenticated &&
+                <i className={`fas fa-heart album-add-to-collection-icon ${this.props.isAlbumIsInCollection ? "master-color" : ''}`}
+                    title={this.props.isAlbumIsInCollection ? "Remove from My Collection" : "Add to My Collection"}
+                    onClick={this.addOrRemoveAlbumFromCollectionHandler}
+                    ref={d => this.heartRef = d}
+                ></i>
+            }
+            <div className="album-holder-play-icon-visible-on-hover">
+                <i className="far fa-eye" title="View this Album" onClick={() => this.albumViewCallBack(this.props.albumId)}></i>
+                <i className="fas fa-play" title="Reset Current Queue and Play this Album" ref={d => this.playRef = d} onClick={() => this.albumPlayCallBack(this.props.albumId)}></i>
+                <i className="fas fa-plus-square" title="Add to The current Queue" onClick={() => this.albumAddToCurrentQueueCallBack(this.props.albumId)}></i>
             </div>
-        )
+            <div className="album-holder-songs-total-holder">{this.props.albumTotalSongs}</div>
+            <div className="album-holder-album-desc">
+                <div className="album-holder-album-title">{this.props.albumName}</div>
+                <div className="album-holder-album-date">{this.props.albumCreationDate}</div>
+                <div className="album-holder-album-by">by {this.props.albumCreatedBy}</div>
+            </div>
+        </div>
+    }
+
+    ExploreMore = () => {
+        return <div className="album-holder-wrapper album-explore-more cursor-pointer" onClick={() => this.props.push(this.props.exploreMoreUrl)}>
+            <div className="explore-more-icon-holer">
+                <i class="fas fa-compass"></i>
+            </div>
+            <div className="explore-more-text-holer">
+                Explore More
+            </div>
+        </div>
+    }
+
+
+    render() {
+        return !this.props.exploreMore ? <this.Main /> : <this.ExploreMore />;
     }
 
 }
