62648d62dc085858d650106a482dcefd5bfa6cb6
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-06-07 02:49:39 +0530

    Added Not Support For Safari

--- services/clientapp/src/actions/offlineActions.js
+++ services/clientapp/src/actions/offlineActions.js
@@ -1,4 +1,6 @@
-import { store } from "../store"
+import {
+    store
+} from "../store"
 
 export const setOnlinestatus = (status = true) => {
     store.dispatch({
@@ -6,9 +8,19 @@ export const setOnlinestatus = (status = true) => {
         payload: status
     })
 }
+export const setSafaristatus = (status = false) => {
+    console.log(status);
+    store.dispatch({
+        type: "SET_SAFARI_STATUS",
+        payload: status
+    })
+}
+
+const isSafari = () => (/^((?!chrome|android).)*safari/i.test(navigator.userAgent));
 
 export const offlineCheckerInitiator = () => {
     window.addEventListener("load", () => {
+        setSafaristatus(isSafari());
         window.addEventListener("online", advancedOfflineChecker);
         window.addEventListener("offline", advancedOfflineChecker);
     });
@@ -17,7 +29,10 @@ export const offlineCheckerInitiator = () => {
 
 export const advancedOfflineChecker = () => {
     const isReachable = (url) => {
-        return fetch(url, { method: 'HEAD', mode: 'no-cors' })
+        return fetch(url, {
+                method: 'HEAD',
+                mode: 'no-cors'
+            })
             .then(function (resp) {
                 return resp && (resp.ok || resp.type === 'opaque');
             })
--- services/clientapp/src/components/Offline.jsx
+++ services/clientapp/src/components/Offline.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import "../assets/css/offline.css";
 import { logoicon } from '../assets/images';
 import { push } from 'connected-react-router';
@@ -9,15 +9,31 @@ class Offline extends Component {
 
     render() {
         return (
-            <div className={`offline-wrapper ${this.props.isOnline ? '' : 'show-offline-wrapper'}`}>
+            <div className={`offline-wrapper ${(this.props.isOnline && !this.props.isSafari) ? '' : 'show-offline-wrapper'}`}>
                 <img src={logoicon} alt="" srcSet="" />
-                <div className="offline-title">
-                    Seems like you are offline!
-               </div>
-                <div className="offline-description">
-                    OpenBeats will automatically resume once you are online!
-                </div>
-                <div className="offline-fallback-block">if not <a href="/">click here</a> to reload</div>
+                {this.props.isSafari
+                    ? (
+                        <Fragment>
+                            <div className="offline-title">
+                                We do not support Safari yet  :(
+                            </div>
+                            <div className="offline-description">
+                                Try OpenBeats using <strong>Google Chrome</strong> to get lifetime access to unlimited songs for free. Hurry now !!!
+                            </div>
+                        </Fragment>
+                    ) : (
+                        <Fragment>
+                            <div className="offline-title">
+                                Seems like you are offline!
+                            </div>
+                            <div className="offline-description">
+                                OpenBeats will automatically resume once you are online!
+                            </div>
+                            <div className="offline-fallback-block">if not <a href="/">click here</a> to reload</div>
+                        </Fragment>
+                    )
+                }
+
             </div>
         );
     }
@@ -26,7 +42,8 @@ class Offline extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        isOnline: state.offlineReducer.isOnline
+        isOnline: state.offlineReducer.isOnline,
+        isSafari: state.offlineReducer.isSafari
     }
 }
 
